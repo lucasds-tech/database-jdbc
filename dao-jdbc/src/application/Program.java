62332b6c503e19604ab936c8849ff905fdef1e03
Lucas Tech <lucas.dasilva09@hotmail.com>
2025-02-21 15:39:52 -0300

    DepartmentDao implementation

--- dao-jdbc/src/application/Program.java
+++ dao-jdbc/src/application/Program.java
@@ -39,7 +39,7 @@ public class Program {
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id = " + newSeller.getId());
 		
-		System.out.println("\n=== TEST 5: seller insert ===");
+		System.out.println("\n=== TEST 5: seller update ===");
 		seller = sellerDao.findById(8);
 		seller.setName("Diogenes Junior");
 		seller.setEmail("diogenes.jr@gmail.com");
